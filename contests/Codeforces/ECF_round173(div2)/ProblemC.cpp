1558c9a26d988e0e4b3934e7bc1d618ed54df3f4
Raisa Zaman <90964545+RaisaZirus@users.noreply.github.com>
2024-12-30 05:17:02 +0600

    hehe

--- contests/Codeforces/ECF_round173(div2)/ProblemC.cpp
+++ contests/Codeforces/ECF_round173(div2)/ProblemC.cpp
@@ -1,47 +1,146 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long int ll;
 //sums on segments 
+ll maxn(ll a, ll b){
+    if(a>b)return a;
+    else return b;
+}
+ll minn(ll a, ll b){
+    if(a>b)return b;
+    else return a;
+}
+ll  max_subarray(vector<int> &arr){
+    ll mx = arr[0];
+    ll current_maxsum = mx;
+    for(int i =1 ; i < arr.size(); i++){
+        current_maxsum = maxn(arr[i], current_maxsum+arr[i]);
+        mx = maxn(mx,current_maxsum);
+    }
+    return mx;
+}
+int min_subarray(vector<int> &arr){
+    ll mx = -1 * arr[0];
+    ll current_maxsum = mx;
+    for(int i =1 ; i < arr.size(); i++){
+        current_maxsum = maxn(-1*arr[i], current_maxsum+ (-1*arr[i]));
+        mx = maxn(mx,current_maxsum);
+    }
+    return -1*mx;
+}
 int main(){
     //freopen("input.txt", "r", stdin);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(NULL);
     int t; cin >> t;
     while(t--){
         int n ; cin >> n;
-        vector<int> v(n); int index; 
+        vector<int> v(n); int index = n/2; 
         for(int i = 0; i <n; i++){
             cin >> v[i];
             if(v[i]!=-1 && v[i]!=1)index = i;
         }
+        if(index>0 && index<n-1){
         vector<int> left_arr(index), right_arr(n-index-1);
         copy(v.begin(), v.begin()+index, left_arr.begin());
         copy(v.begin()+index+1, v.end(), right_arr.begin());
-        for(auto u: right_arr)cout << u << " ";
-        v.clear(); left_arr.clear(); right_arr.clear();
+        //for(auto u: right_arr)cout << u << " ";
+        //now apply kadane's algo
+        set<int> answer;
+        //cout<< min_subarray(left_arr) << endl ;
+        int mx1 = max_subarray(left_arr), mx2 = max_subarray(right_arr);
+        int mn1 = min_subarray(left_arr), mn2 = min_subarray(right_arr);
+        int uplim = max(mx1, mx2);
+        for(int i = min(mn1, mn2); i<=uplim;i++)answer.insert(i);
+        //now with the extra element
+        answer.insert(v[index]);
+        int mxp = right_arr[0],mnp = right_arr[0];
+        for(int i = 1; i<right_arr.size(); i++){
+            right_arr[i]+=right_arr[i-1];
+            mxp = max(right_arr[i], mxp);
+            mnp = min(right_arr[i], mnp);
+        }
+        int mxs = left_arr[left_arr.size()-1],mns = left_arr[left_arr.size()-1];
+        for(int i = left_arr.size()-2; i>=0; i--){
+            left_arr[i]+=left_arr[i+1];
+            mxs = max(left_arr[i], mxs);
+            mns = min(left_arr[i], mns);
+        }
+        //cout << mns << endl;
+        answer.insert(0);
+        uplim = v[index]+ max(max(mxp,mxs),mxp+mxs);
+        int min_lim = min(min(mnp,mns),mnp+mns);
+        for(int i = min_lim+v[index]; i<= uplim;i++)answer.insert(i);
+        cout << answer.size() << "\n";
+        for(auto u:answer)cout << u << " ";
+        cout << "\n";
+        v.clear(); left_arr.clear(); right_arr.clear(); answer.clear();
+        }
+        else if(index == 0){//-------------------------------------------------------
+            //only right array count
+        vector<int>  right_arr(n-index-1);
+        copy(v.begin()+index+1, v.end(), right_arr.begin());
+        //for(auto u: right_arr)cout << u << " ";
+        //now apply kadane's algo
+        set<int> answer;
+        //cout<< min_subarray(left_arr) << endl ;
+        int mx2 = max_subarray(right_arr);
+        int mn2 = min_subarray(right_arr);
+        int uplim = mx2;
+        for(int i = mn2; i<=uplim;i++)answer.insert(i);
+        //now with the extra element
+        answer.insert(v[index]);
+        int mxp = right_arr[0],mnp = right_arr[0];
+        for(int i = 1; i<right_arr.size(); i++){
+            right_arr[i]+=right_arr[i-1];
+            mxp = max(right_arr[i], mxp);
+            mnp = min(right_arr[i], mnp);
+        }
+        //cout << mns << endl;
+        answer.insert(0);
+        uplim = v[index]+mxp;
+        int min_lim = mnp;
+        for(int i = min_lim+v[index]; i<= uplim;i++)answer.insert(i);
+        cout << answer.size() << "\n";
+        for(auto u:answer)cout << u << " ";
+        cout << "\n";
+        v.clear(); right_arr.clear(); answer.clear();
+        }
+        else if(index==n-1){//-------------------------------------------------------
+        vector<int> left_arr(index);
+        copy(v.begin(), v.begin()+index, left_arr.begin());
+        //for(auto u: right_arr)cout << u << " ";
+        //now apply kadane's algo
+        set<int> answer;
+        //cout<< min_subarray(left_arr) << endl ;
+        int mx1 = max_subarray(left_arr);
+        int mn1 = min_subarray(left_arr);
+        int uplim = mx1;
+        for(int i = mn1; i<=uplim;i++)answer.insert(i);
+        //now with the extra element
+        answer.insert(v[index]);
+        
+        int mxs = left_arr[left_arr.size()-1],mns = left_arr[left_arr.size()-1];
+        for(int i = left_arr.size()-2; i>=0; i--){
+            left_arr[i]+=left_arr[i+1];
+            mxs = max(left_arr[i], mxs);
+            mns = min(left_arr[i], mns);
+        }
+        //cout << mns << endl;
+        answer.insert(0);
+        uplim = v[index]+ mxs;
+        int min_lim = mns;
+        for(int i = min_lim+v[index]; i<= uplim;i++)answer.insert(i);
+        
+        cout << answer.size() << "\n";
+        
+        for(auto u:answer)cout << u << " ";
+        cout << "\n";
+        v.clear(); left_arr.clear(); answer.clear();
+            
+        }
     }   
 }
 /*
-5
-5
-1 -1 10 1 1
-for this test case, suppose you forget about the different element 10 and just think about 
-the two portions-> the portions have a max subarray sum and a min subarray sum.
-If you look closely, you can find that if you somehow find max and min sum, all the numbers between them
-can be from a subarray. suppose currently you have a subarray sum k, you add another element which is 
-either 1 or -1, so, the sum value would increase by one or decrease by one, if you keep cutting elements
-left and right to reach the minimum sum, you'll be getting every number possible till the min sum, same 
-goes for max. therefore, [min, max] has all the integers that you can get from those two arrays.
-
-
-
-
-5
--1 -1 -1 -1 -1
-2
--1 2
-2
-7 1
-3
-1 4 -1
-
+https://codeforces.com/contest/2043/problem/C
 */
